3d961612eddd7f2ca269b87a582fe00a1eb04972
goeddie <goeddieuk@gmail.com>
2015-01-07 21:44:49 +0000

    initial project skeleton

---
+++ SqlPackageFilter/DeploymentFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Dac.Deployment;
+
+namespace AgileSqlClub.SqlPackageFilter
+{
+    [ExportDeploymentPlanModifier("AgileSqlClub.DeploymentFilterContributor", "0.1.0.0")]
+    public class DeploymentFilter
+    {
+
+    }
+}
