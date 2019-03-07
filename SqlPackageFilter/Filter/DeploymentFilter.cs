81931e3b33903920abfd7296a6adf152c4f997c6
Ed Elliott <goeddieuk@gmail.com>
2019-03-07 21:14:50 +0000

    references

--- SqlPackageFilter/Filter/DeploymentFilter.cs
+++ SqlPackageFilter/Filter/DeploymentFilter.cs
@@ -7,7 +7,7 @@ using Microsoft.SqlServer.Dac.Extensibility;
 
 namespace AgileSqlClub.SqlPackageFilter.Filter
 {
-  [ExportDeploymentPlanModifier("AgileSqlClub.DeploymentFilterContributor", "1.4.3.0")]
+  [ExportDeploymentPlanModifier("AgileSqlClub.DeploymentFilterContributor", "1.4.3.1")]
   public class DeploymentFilter : DeploymentPlanModifier, IDisplayMessageHandler
   {
     private DisplayMessageLevel _displayLevel = DisplayMessageLevel.Errors;
